2f689bad6063a14941d40dcb2f41d8a3259afef5
Estevan Hernandez <esther65536@gmail.com>
2023-11-24 18:42:27 -0600

    clean up TypedGArray, move GArray extension to its own file

--- Sources/SwiftGodot/Extensions/TypedGArray.swift
+++ Sources/SwiftGodot/Extensions/TypedGArray.swift
@@ -5,8 +5,11 @@
 //  Created by Estevan Hernandez on 11/24/23.
 //
 
+/// This struct is primarily used to facilitate the `@Export` of `Array` types whose elements conform to `VariantRepresentable`
+/// It acts as a wrapper for the Swift Array and manages an underlying `GArray`
+/// `@Export exportedArray: Array<Node> = []`
 public struct TypedGArray<T: VariantRepresentable> {
-	public var gArray: GArray
+	public private(set) var gArray: GArray
 
 	private var _array: [T]
 	public var array: [T] {
@@ -24,21 +27,5 @@ public struct TypedGArray<T: VariantRepresentable> {
 	public init(_ array: inout [T]) {
 		self.gArray = GArray.make(array)
 		self._array = array
-	}
-}
-
-public extension GArray {
-	static func empty<T: VariantRepresentable>(_ type: T.Type = T.self) -> GArray {
-		GArray( base: GArray(), type: Int32(T.godotType.rawValue), className: StringName("\(T.self)"), script: Variant())
-	}
-	
-	static func make<T: VariantRepresentable>(_ array: [T]) -> GArray {
-		array.reduce(into: empty(T.self)) {
-			$0.append(value: Variant($1))
-		}
-	}
-	
-	func asArray<T: VariantRepresentable>(_ type: T.Type = T.self) -> [T] {
-		compactMap { T($0) }
 	}
 }
