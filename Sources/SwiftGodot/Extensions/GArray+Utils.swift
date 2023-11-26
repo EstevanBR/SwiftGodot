61599430f49348a1b2eb5b39d3c51bc4075a325a
Estevan Hernandez <esther65536@gmail.com>
2023-11-26 02:57:05 -0600

    Revert "revert this"
    This reverts commit 840f1052de318d2b63b8149977e1b9446f7c6553.

--- Sources/SwiftGodot/Extensions/GArray+Utils.swift
+++ Sources/SwiftGodot/Extensions/GArray+Utils.swift
@@ -18,25 +18,12 @@ public extension GArray {
 
 	/// Creates a GArray whose elements are from the passed array, and whose elements conform to `VariantRepresentable`
 	static func make<T: VariantRepresentable>(_ array: [T]) -> GArray {
-		GD.print("Making GArray from \(array)\n   At:\(#fileID):\(#line)")
-		let result = array.reduce(into: empty(T.self)) {
+		array.reduce(into: empty(T.self)) {
 			$0.append(value: Variant($1))
 		}
-		GD.print("Made GArray: \(result.debugDescription))\n   At:\(#fileID):\(#line)")
-		return result
 	}
 }
 
 public extension Array where Element: VariantRepresentable {
 	var gArray: GArray { self.reduce(into: .empty(Element.self)) { $0.append(value: Variant($1)) } }
-}
-
-extension GArray: CustomDebugStringConvertible {
-	public var debugDescription: String {
-		"""
-		count: \(self.count)
-		isTyped: \(self.isTyped())
-		mapped: \(self.compactMap { "\($0.debugDescription)" }.joined(separator: ", "))
-		"""
-	}
 }
